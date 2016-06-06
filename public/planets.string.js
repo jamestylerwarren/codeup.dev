8fd13fe869a110b7506906713196333c74f2ea42
Tyler Warren <warrejt@yahoo.com>
2016-06-05 22:37:53 -0500

    plaenet string array exercise

--- public/planets.string.js
+++ public/planets.string.js
@@ -1,21 +1,32 @@
 (function(){
     "use strict";
 
-    var planetsString = "Mercury|Venus|Earth|Mars|Jupiter|Saturn|Uranus|Neptune";
+    var planetsString = "Mercury,Venus,Earth,Mars,Jupiter,Saturn,Uranus,Neptune";
     var planetsArray;
 
     // TODO: Convert planetsString to an array, save it to planetsArray.
-    var planetsArray = planetsString.split(",");
+    planetsArray = planetsString.split(",");
 
     console.log(planetsArray);
 
     // TODO: Create a string with <br> tags between each planet. console.log() your results.
     //       Why might this be useful?
 
+    var planetString = "Mercury<br>Venus<br>Earth<br>Mars<br>Jupiter<br>Saturn<br>Uranus<br>Neptune";
 
+    var planetArray = planetString.split(",");
 
+    console.log(planetArray);
 
     // Bonus: Create a second string that would display your planets in an undordered list.
     //        You will need an opening AND closing <ul> tags around the entire string, and <li> tags around each planet.
     //        console.log() your results.
+
+
+    var planetStringTwo = "<ul><li>Mercury</li><li>Venus</li><li>Earth</li><li>Mars</li><li>Jupiter</li><li>Saturn</li><li>Uranus</li><li>Neptune</li></ul>";
+
+    var planetArrayTwo = planetStringTwo.split(",");
+
+    console.log(planetArrayTwo);
+
 })();
