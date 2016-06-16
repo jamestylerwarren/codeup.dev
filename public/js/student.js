dc82fd28dadbe08cb606b8b9579032231f6c3cf5
Tyler Warren <warrejt@yahoo.com>
2016-06-16 11:54:58 -0500

    jquery - student grades converting to jquery

--- public/js/student.js
+++ public/js/student.js
@@ -1,44 +1,44 @@
 'use strict';
 // Buttons are disabled upon page load
-document.getElementById("add-grade").disabled = true;
-document.getElementById("calculate-average").disabled = true;
+$("#add-grade").attr('disabled', 'true');
+$("#calculate-average").attr('disabled', 'true');
 
 // Adding Listeners
-var saveButton = document.getElementById("save-name");
-saveButton.addEventListener('click', getName, false);
+var saveButton = $("#save-name");
+saveButton.click('click', getName);
 
-var addAndContinue = document.getElementById("add-grade");
-addAndContinue.addEventListener('click', addSubject, false);
+var addAndContinue = $("#add-grade");
+addAndContinue.click('click', addSubject);
 
-var addAndCalculate = document.getElementById("calculate-average");
-addAndCalculate.addEventListener('click', calculateAverage, false);
+var addAndCalculate = $("#calculate-average");
+addAndCalculate.click('click', calculateAverage);
 
 
 
 // getting name value and sending it to the variable student.name to store it
 function getName (event){
 	var students = " ";
-	var student_name = document.getElementById("student-name").value;
+	var student_name = $("#student-name").value;
 	student_name = student.name 
 	// enabling buttons when value is entered into name field
-	document.getElementById("add-grade").disabled = false;
-	document.getElementById("calculate-average").disabled = false;
-	document.getElementById("student-name").innerText = xstudent.name;
+	$("#add-grade").removeAttr('disabled');
+	$("#calculate-average").removeAttr('disabled');
+	$("#student-name").text(student.name);
 }
 
 
 function addSubject (event){
 	var subjectName = "";
 	//calling subject field; saving the value as a string and sending to subject.name
-	subjectName = document.getElementById("subject").value;
-	document.getElementById("subject").value = "";
+	subjectName = $("#subject").val();
+	$("#subject").val("");
 	var grade = "";
 	//calling grade field; saving the value (as a number) as a string
-	grade = parseInt(document.getElementById("grade").value);
-	document.getElementById("grade").value = "";
+	grade = parseInt($("#grade").val());
+	$("#grade").val("");
 	// sending grade to the table
-	document.getElementById("grades").innerHTML += "<tr><td>" + subjectName 
-	+ "</td><td>" + grade + "</td></tr>";
+	$("#grades").html($("#grades").html() + "<tr><td>" + subjectName 
+	+ "</td><td>" + grade + "</td></tr>");
 	//sending subject name and grade in the () to the addSubject function in the student variable
 	student.addSubject(subjectName, grade); 
 }
@@ -48,12 +48,12 @@ function calculateAverage (event){
 	//otherwise it won't take the data in subj and grade fields	
 	addSubject()
 	var average = student.calculateAverage()
-	document.getElementById("student-average").innerText = average;
+	$("#student-average").text(average);
 	//if statement evaluating wether the great is awesome or not
 	if (student.isAwesome()) {
-			document.getElementById("student-awesome").className = "";
+			$("#student-awesome").removeAttr('class');
 		} else {
-			document.getElementById("student-practice").className = "";
+			$("#student-practice").removeAttr('class');
 		}
 
 }
