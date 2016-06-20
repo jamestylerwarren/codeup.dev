ce15964e8fcb785107bd41ecec3e63ff2a0e2518
Tyler Warren <warrejt@yahoo.com>
2016-06-20 09:40:04 -0500

    weather api exercise update

--- public/js/weather_map.js
+++ public/js/weather_map.js
@@ -4,33 +4,39 @@
 
 
 
-// $.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-//     APPID: "6591e56be16ed754b9738e7806c46382",
-//     q:     "San Antonio, TX",
-//     units: "imperial"
-// }).done(function(data) {
-//     console.log(data);
-// }).fail(function(){
-// 	alert('There was an error');
-// }).always(function(){
-// 	alert('All done');
-// });
+	$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+		APPID: "6591e56be16ed754b9738e7806c46382",
+		lat: 29.4259669,
+		lon: -98.4893873,
+		units: "imperial"
+	}).done(function(weatherData) {
+		console.log(weatherData);
+		$('#dayZeroForecast').append(weatherData.list[0].temp.max + '°/' + weatherData.list[0].temp.min + '°<br>Clouds: ' + weatherData.list[0].clouds + '<br>Humidity: ' + weatherData.list[0].humidity);
+		
+
+
+		
+	}).fail(function(){
+		alert('There was an error');
+	}).always(function(){
+		alert('All done');
+	});
 
 
 // Set our map options
-        var mapOptions = {
-            // Set the zoom level
-            zoom: 10,
+	var mapOptions = {
+		// Set the zoom level
+		zoom: 10,
 
-            // This sets the center of the map at our location
-            center: {
-                lat:  29.426791,
-                lng: -98.489602
-            }
-        };
+		// This sets the center of the map at our location
+		center: {
+			lat:  29.426791,
+			lng: -98.489602
+		}
+	};
 
-        // Render the map
-        var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	// Render the map
+	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
 
 
 
