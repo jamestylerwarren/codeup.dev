55b86f00e3bd54143325844b8c0c959af66d54f6
Tyler Warren <warrejt@yahoo.com>
2016-06-20 15:10:17 -0500

    weather map exercise

--- public/js/weather_map.js
+++ public/js/weather_map.js
@@ -2,41 +2,64 @@
 "use strict";
 
 
+		//------------Map Rendering---------------
+	var marker;
+	function setMap() {
+		var map = new google.maps.Map(document.getElementById("map"), {
+			zoom: 10,
+			center: {
+				lat: 29.426791,
+				lng: -98.489602
+			}
+		});
+		marker = new google.maps.Marker({
+			position: map.center,
+			map: map,
+			draggable: true
+		});		
+		marker.addListener('dragend',function(event) {
+		getWeather();
+        });
+	}; setMap();
 
 
-	$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
-		APPID: "6591e56be16ed754b9738e7806c46382",
-		lat: 29.4259669,
-		lon: -98.4893873,
-		units: "imperial"
-	}).done(function(weatherData) {
-		console.log(weatherData);
-		$('#dayZeroForecast').append(weatherData.list[0].temp.max + '°/' + weatherData.list[0].temp.min + '°<br>Clouds: ' + weatherData.list[0].clouds + '<br>Humidity: ' + weatherData.list[0].humidity);
+	//---------Weather Forecast-----------
+	
+	function getWeather() {
+		$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
+			APPID: "6591e56be16ed754b9738e7806c46382",
+			lat: marker.getPosition().lat(),
+			lon: marker.getPosition().lng(),
+			units: "imperial",
+			cnt: 3
+		}).done(function(weatherData) {
+			weatherData.list.forEach( function (forecast, i){
+			var maxTemp = forecast.temp.max;
+			var minTemp = forecast.temp.min;
+			var icon ='<img src="http://openweathermap.org/img/w/'+forecast.weather[0].icon+'.png"';
+			var description = forecast.weather[0].description;
+			var humidity = forecast.humidity;
+			var speed = forecast.speed;
+			var pressure = forecast.pressure;
+			$('#dayForecast' + i).html(maxTemp + '° / ' + minTemp + '°<br>' + icon + '<br><br>' + description + '<br><br>Humidity: ' + humidity + ' %<br>Wind Speed: ' + speed + ' mph<br>Pressure: ' + pressure);
+			});
+		});
+	}; getWeather();
+
+
+	
 		
+	
+	
 
 
-		
-	}).fail(function(){
-		alert('There was an error');
-	}).always(function(){
-		alert('All done');
-	});
+	
 
 
-// Set our map options
-	var mapOptions = {
-		// Set the zoom level
-		zoom: 10,
 
-		// This sets the center of the map at our location
-		center: {
-			lat:  29.426791,
-			lng: -98.489602
-		}
-	};
 
-	// Render the map
-	var map = new google.maps.Map(document.getElementById("map"), mapOptions);
+	
+	
 
 
 
