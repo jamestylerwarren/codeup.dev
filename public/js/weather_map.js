d439bac4b6045a642ad3778b2c52c6a041e3a95f
Tyler Warren <warrejt@yahoo.com>
2016-06-21 09:40:31 -0500

    Weather app exercise

--- public/js/weather_map.js
+++ public/js/weather_map.js
@@ -1,6 +1,6 @@
 (function() {
 "use strict";
-		//------------Map Rendering---------------
+		//------------Map Rendering------------//
 	var marker;
 	function setMap() {
 		var map = new google.maps.Map(document.getElementById("map"), {
@@ -9,7 +9,7 @@
 				lat: 29.426791,
 				lng: -98.489602
 			},
-			styles: [{"featureType":"administrative","elementType":"labels.text.fill","stylers":[{"color":"#444444"}]},{"featureType":"landscape","elementType":"all","stylers":[{"color":"#f2f2f2"}]},{"featureType":"poi","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"road","elementType":"all","stylers":[{"saturation":-100},{"lightness":45}]},{"featureType":"road.highway","elementType":"all","stylers":[{"visibility":"simplified"}]},{"featureType":"road.arterial","elementType":"labels.icon","stylers":[{"visibility":"off"}]},{"featureType":"transit","elementType":"all","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"all","stylers":[{"color":"#4f595d"},{"visibility":"on"}]}]
+			styles: [{"elementType":"geometry","stylers":[{"hue":"#ff4400"},{"saturation":-68},{"lightness":-4},{"gamma":0.72}]},{"featureType":"road","elementType":"labels.icon"},{"featureType":"landscape.man_made","elementType":"geometry","stylers":[{"hue":"#0077ff"},{"gamma":3.1}]},{"featureType":"water","stylers":[{"hue":"#00ccff"},{"gamma":0.44},{"saturation":-33}]},{"featureType":"poi.park","stylers":[{"hue":"#44ff00"},{"saturation":-23}]},{"featureType":"water","elementType":"labels.text.fill","stylers":[{"hue":"#007fff"},{"gamma":0.77},{"saturation":65},{"lightness":99}]},{"featureType":"water","elementType":"labels.text.stroke","stylers":[{"gamma":0.11},{"weight":5.6},{"saturation":99},{"hue":"#0091ff"},{"lightness":-86}]},{"featureType":"transit.line","elementType":"geometry","stylers":[{"lightness":-48},{"hue":"#ff5e00"},{"gamma":1.2},{"saturation":-23}]},{"featureType":"transit","elementType":"labels.text.stroke","stylers":[{"saturation":-64},{"hue":"#ff9100"},{"lightness":16},{"gamma":0.47},{"weight":2.7}]}]
 			});
 		marker = new google.maps.Marker({
 			position: map.center,
@@ -22,7 +22,7 @@
         });
 	}; setMap();
 
-	//---------Weather Forecast-----------	
+	//---------Weather Forecast----------//
 	function getWeather() {
 		$.get("http://api.openweathermap.org/data/2.5/forecast/daily", {
 			APPID: "6591e56be16ed754b9738e7806c46382",
@@ -45,9 +45,6 @@
 	}; getWeather();
 
 
-	
-		
-	
 	
 
 
