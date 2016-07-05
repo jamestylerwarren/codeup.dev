3fc0ffca61e8411d2127ddac2ba982c08feb63f3
Tyler Warren <warrejt@yahoo.com>
2016-07-05 09:22:24 -0500

    iterating.js refactored using PHP

---
+++ public/iterating.php
@@ -0,0 +1,12 @@
+<?php
+
+	$names = ['tyler', 'chris', 'randi', 'eddie'];
+
+
+	echo 'There are ' . count($names) . ' names in this array.' . PHP_EOL;
+
+
+	echo $names[0] . PHP_EOL;
+	echo $names[1] . PHP_EOL;
+	echo $names[2] . PHP_EOL;
+	echo $names[3] . PHP_EOL;
