1a1c760bdc20800abe64f7eebaab248952e25724
Tyler Warren <warrejt@yahoo.com>
2016-07-13 14:50:03 -0500

    created arrays

---
+++ public/server-name-generator.php
@@ -0,0 +1,5 @@
+<?php
+
+$adjectives = ['crazy', 'salty', 'insane', 'robust', 'rotund', 'sick', 'worthless', 'dumb', 'michevious', 'talkative'];
+
+$nouns = ['cucumber', 'phone', 'wife', 'friend', 'food', 'codeup', 'store', 'dog', 'computer', 'car'];
