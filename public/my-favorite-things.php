1530cbe9032d19c9ca784f559afb2f8c270c4bb6
Tyler Warren <warrejt@yahoo.com>
2016-07-13 15:18:12 -0500

    finised server name and started fav things

---
+++ public/my-favorite-things.php
@@ -0,0 +1,15 @@
+<?php
+
+$favThings = ['crazy', 'salty', 'insane', 'robust', 'hairy', 'misinformed', 'worthless', 'dumb', 'michevious', 'talkative'];
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+</body>
+</html>
--- public/server-name-generator.php
+++ public/server-name-generator.php
@@ -1,5 +1,24 @@
 <?php
 
-$adjectives = ['crazy', 'salty', 'insane', 'robust', 'rotund', 'sick', 'worthless', 'dumb', 'michevious', 'talkative'];
+$adjectives = ['crazy', 'salty', 'insane', 'noisy', 'hairy', 'misinformed', 'worthless', 'dumb', 'michevious', 'talkative'];
 
 $nouns = ['cucumber', 'phone', 'wife', 'friend', 'food', 'codeup', 'store', 'dog', 'computer', 'car'];
+
+function randomGenerator($adjectives, $nouns) {
+	$noun = array_rand($nouns);
+	$adjective = array_rand($adjectives);
+	echo $adjectives[$adjective] . ' ' . $nouns[$noun];
+}
+
+
+?>
+<!DOCTYPE html>
+
+<html>
+<head>
+	<title>Random Generator</title>
+</head>
+<body>
+	<h1><?php echo randomGenerator($adjectives, $nouns) ?></h1>
+</body>
+</html>
