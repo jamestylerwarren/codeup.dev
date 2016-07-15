07589f1fc0ab2f1eeed6b4abec4332a206dd1f06
Tyler Warren <warrejt@yahoo.com>
2016-07-15 14:40:25 -0500

    styled login page with bootstrap and bootsnipp

--- public/login.php
+++ public/login.php
@@ -27,16 +27,121 @@ extract(pagecontroller());
 
 	<!-- Bootstrap Core CSS -->
 	<link href="css/bootstrap.min.css" rel="stylesheet">
-	
+
+	<!-- Font Awesome if needed -->
+	<link href="font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+
 	<title>Login Page</title>
+
+	<style>
+		body {
+			padding-top: 120px;
+			padding-bottom: 40px;
+			background-color: #eee;
+		  
+		}
+		.btn {
+			outline:0;
+			border:none;
+			border-top:none;
+			border-bottom:none;
+			border-left:none;
+			border-right:none;
+			box-shadow:inset 2px -3px rgba(0,0,0,0.15);
+		}
+		.btn:focus{
+			outline:0;
+			-webkit-outline:0;
+			-moz-outline:0;
+		}
+		.fullscreen_bg {
+			position: fixed;
+			top: 0;
+			right: 0;
+			bottom: 0;
+			left: 0;
+			background-size: cover;
+			background-position: 50% 50%;
+			background-image: url('img/mez4hEM.gif');
+			background-repeat:repeat;
+		}
+		.form-signin {
+			max-width: 280px;
+			padding: 15px;
+			margin: 0 auto;
+			  margin-top:50px;
+		}
+		.form-signin .form-signin-heading, .form-signin {
+			margin-bottom: 10px;
+		}
+		.form-signin .form-control {
+			position: relative;
+			font-size: 16px;
+			height: auto;
+			padding: 10px;
+			-webkit-box-sizing: border-box;
+			-moz-box-sizing: border-box;
+			box-sizing: border-box;
+		}
+		.form-signin .form-control:focus {
+			z-index: 2;
+		}
+		.form-signin input[type="text"] {
+			margin-bottom: -1px;
+			border-bottom-left-radius: 0;
+			border-bottom-right-radius: 0;
+			border-top-style: solid;
+			border-right-style: solid;
+			border-bottom-style: none;
+			border-left-style: solid;
+			border-color: #000;
+		}
+		.form-signin input[type="password"] {
+			margin-bottom: 10px;
+			border-top-left-radius: 0;
+			border-top-right-radius: 0;
+			border-top-style: none;
+			border-right-style: solid;
+			border-bottom-style: solid;
+			border-left-style: solid;
+			border-color: rgb(0,0,0);
+			border-top:1px solid rgba(0,0,0,0.08);
+		}
+		.form-signin-heading {
+			color: #fff;
+			text-align: center;
+			text-shadow: 0 2px 2px rgba(0,0,0,0.5);
+		}
+
+
+	</style>
 </head>
 <body>
-	<h1>Please enter login info</h1>
-	<!-- <p><?= $message ?></p> -->
-	<form method="POST">
-        <input type="text" name="username" placeholder="Username"><br>
-        <input type="password" name="password" placeholder="Password"><br>
-        <input type="submit">
-    </form>
+  
+	<div id="fullscreen_bg" class="fullscreen_bg"/>
+
+		<div class="container">
+
+			<form class="form-signin" method="POST">
+				<h1 class="form-signin-heading text-muted">Sign In</h1>
+				<input type="text" name="username" class="form-control" placeholder="Username" required="" autofocus="">
+				<input type="password" name="password" class="form-control" placeholder="Password" required="">
+				<button class="btn btn-lg btn-primary btn-block" type="submit">Sign In</button>
+			</form>
+
+		</div>
+	</div>
+
+
+
+	<!-- jQuery -->
+	<script src="js/jquery.js"></script>
+
+	<!-- Bootstrap Core JavaScript -->
+	<script src="js/bootstrap.min.js"></script>
+
+	<!-- Plugin JavaScript -->
+	<script src="http://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 </body>
 </html>
