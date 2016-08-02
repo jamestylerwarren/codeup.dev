e9df0d9c30b7c18bf3f69f4fee3142a5c852aca5
Tyler Warren <warrejt@yahoo.com>
2016-08-02 11:15:20 -0500

    working on adding tables and info into table

--- db_connect.php
+++ db_connect.php
@@ -7,3 +7,17 @@ $dbc = new PDO('mysql:host='.DB_HOST.';dbname='.DB_NAME, DB_USER, DB_PASS);
 $dbc->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
 echo $dbc->getAttribute(PDO::ATTR_CONNECTION_STATUS) . "\n";
+
+$users = [
+    ['email' => 'jason@codeup.com',   'name' => 'Jason Straughan'],
+    ['email' => 'chris@codeup.com',   'name' => 'Chris Turner'],
+    ['email' => 'michael@codeup.com', 'name' => 'Michael Girdley']
+];
+
+foreach ($users as $user) {
+    $query = "INSERT INTO users (email, name) VALUES ('{$user['email']}', '{$user['name']}')";
+
+    $dbc->exec($query);
+
+    echo "Inserted ID: " . $dbc->lastInsertId() . PHP_EOL;
+}
